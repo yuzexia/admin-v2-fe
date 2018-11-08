c7405194139ab426de6fa5a1f3698ffca9b1d5b2
yuze.xia <327932801@qq.com>
2018-11-08 14:38:00 +0800

    top nav

--- src/component/nav-side/index.jsx
+++ src/component/nav-side/index.jsx
@@ -6,36 +6,37 @@
  */
 
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 class NavSide extends React.Component {
     render() {
         return (
-            <div class="navbar-default navbar-side" role="navigation">
-                <div class="sidebar-collapse">
-                    <ul class="nav" id="main-menu">
-
+            <div className="navbar-default navbar-side" role="navigation">
+                <div className="sidebar-collapse">
+                    <ul className="nav" id="main-menu">
                         <li>
-                            <a class="active-menu" href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a>
+                            <Link className="active-menu" to="index.html"><i className="fa fa-dashboard"></i> Dashboard</Link>
                         </li>
                         <li>
-                            <a href="ui-elements.html"><i class="fa fa-desktop"></i> UI Elements</a>
+                            <a href="ui-elements.html"><i className="fa fa-desktop"></i> UI Elements</a>
                         </li>
                         <li>
-                            <a href="chart.html"><i class="fa fa-bar-chart-o"></i> Charts</a>
+                            <a href="chart.html"><i className="fa fa-bar-chart-o"></i> Charts</a>
                         </li>
                         <li>
-                            <a href="tab-panel.html"><i class="fa fa-qrcode"></i> Tabs &amp; Panels</a>
+                            <a href="tab-panel.html"><i className="fa fa-qrcode"></i> Tabs &amp; Panels</a>
                         </li>
                         
                         <li>
-                            <a href="table.html"><i class="fa fa-table"></i> Responsive Tables</a>
+                            <a href="table.html"><i className="fa fa-table"></i> Responsive Tables</a>
                         </li>
                         <li>
-                            <a href="form.html"><i class="fa fa-edit"></i> Forms </a>
+                            <a href="form.html"><i className="fa fa-edit"></i> Forms </a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level collapse">
+                            <a href="#"><i className="fa fa-sitemap"></i> Multi-Level Dropdown<span className="fa arrow"></span></a>
+                            <ul className="nav nav-second-level collapse">
                                 <li>
                                     <a href="#">Second Level Link</a>
                                 </li>
@@ -43,8 +44,8 @@ class NavSide extends React.Component {
                                     <a href="#">Second Level Link</a>
                                 </li>
                                 <li>
-                                    <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level collapse">
+                                    <a href="#">Second Level Link<span className="fa arrow"></span></a>
+                                    <ul className="nav nav-third-level collapse">
                                         <li>
                                             <a href="#">Third Level Link</a>
                                         </li>
@@ -61,7 +62,7 @@ class NavSide extends React.Component {
                             </ul>
                         </li>
                         <li>
-                            <a href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
+                            <a href="empty.html"><i className="fa fa-fw fa-file"></i> Empty Page</a>
                         </li>
                     </ul>
                 </div>
