48b8c357e1ad004c760a959ae7c3f553b6bbded3
yuze.xia <327932801@qq.com>
2018-11-10 10:34:51 +0800

    add base frame

---
+++ src/util/mm.jsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: yuze.xia 
+ * @Date: 2018-11-10 10:33:07 
+ * @Last Modified by: yuze.xia
+ * @Last Modified time: 2018-11-10 10:33:28
+ */
+
+class MUtil {
+
+}
+
+export default MUtil;
