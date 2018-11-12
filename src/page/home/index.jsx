f6cbe1012af811de43411368bec5168e35d1d9c5
yuze.xia <327932801@qq.com>
2018-11-12 23:01:01 +0800

    home page

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -6,19 +6,59 @@
  */
 
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PageTitle from 'component/page-title/index.jsx';
 
-import './index.css';
+import './index.scss';
 
 class Home extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userCount: '123',
+            productCount: '456',
+            orderCount: '789'
+        }
+    }
+
+    loadCount() {
+        
+    }
+
     render() {
         return (
             <div id="page-wrapper">
                 <PageTitle title="首页"></PageTitle>
                 <div className="row">
-                    <div className="col-md-12">
-                        body
+                    <div className="col-md-4">
+                        <Link to="/user" className="color-box brown">
+                            <p className="count">{this.state.userCount}</p>
+                            <p className="desc">
+                                <i className="fa fa-user-o"></i>
+                                <span>用户总数</span>
+                            </p>
+                        </Link>
                     </div>
+                    <div className="col-md-4">
+                        <Link to="/user" className="color-box green">
+                            <p className="count">{this.state.userCount}</p>
+                            <p className="desc">
+                                <i className="fa fa-list"></i>
+                                <span>用户总数</span>
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="col-md-4">
+                        <Link to="/user" className="color-box blue">
+                            <p className="count">{this.state.userCount}</p>
+                            <p className="desc">
+                                <i className="fa fa-check-square-o"></i>
+                                <span>用户总数</span>
+                            </p>
+                        </Link>
+                    </div>
+                    
                 </div>
             </div>
         );
